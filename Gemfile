70ac5e2438a60510572ffbb730be8e430d4d1d32
Eric Haughee <ehaughee@gmail.com>
2016-07-15 14:56:07 -0700

    adding redis

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 ruby '2.2.4'
 
 gem 'sinatra'
-gem 'rake' 
+gem 'rake'
+gem 'redis'
--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'redis'
+
+redis = Redis.new(host: "127.0.0.1", port: 6379, db: 0)
+redis.set("test", "wicked sick")
 
 get '/' do
-    "Hello, World!"
+    msg = redis.get("test")
+    "Hello, World! Message: #{msg}"
 end
