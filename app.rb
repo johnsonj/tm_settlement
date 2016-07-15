3f65e8d9e9a9c6eb3ce10ae64c044e2574e9f837
Eric Haughee <ehaughee@gmail.com>
2016-07-15 16:42:40 -0700

    Adding haml views, and round trip redis tests

--- app.rb
+++ app.rb
@@ -1,10 +1,25 @@
 require 'sinatra'
 require 'redis'
+require 'haml'
+require 'json'
 
 redis = Redis.new(url: ENV['REDIS_URL'])
 redis.set("test", "wicked sick")
 
+EMAILS_HASH_NAME = "emails"
+
 get '/' do
     msg = redis.get("test")
-    "Hello, World! Message: #{msg}"
+    haml :index, locals: { msg: msg }
+end
+
+post '/subscribe' do
+    email = params[:email]
+    redis.hincrby(EMAILS_HASH_NAME, email, 1)
+    "Thanks"
+end
+
+get '/emails' do
+    emails = redis.hkeys(EMAILS_HASH_NAME)
+    "#{emails}"
 end
