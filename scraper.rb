c401320bd2966d71a1a333bf69b64873a4b45a4b
Eric Haughee <ehaughee@gmail.com>
2016-08-15 20:48:56 -0700

    Rarranging scraper, styling events

--- scraper.rb
+++ scraper.rb
@@ -8,12 +8,64 @@ SETTLEMENT_PATH = "/microsite/settlement"
 EVENTS_HASH_NAME = "events"
 
 browser = Watir::Browser.new :phantomjs
-redis = Redis.new(url: ENV['REDIS_URL'])
+@redis = Redis.new(url: ENV['REDIS_URL'])
 
 def goto_page(browser, url)
   browser.goto url
   page = Nokogiri::HTML.parse browser.html
   return page
+end
+
+def process_results(results)
+  new_events = []
+  results.each do |result|
+    month_abbr = result.css(".month abbr > text()").text
+    month = result.css(".month abbr").first["title"]
+    date = result.css("td.date div.date").first.text
+    day = result.css(".day abbr").first["title"]
+
+    venue = result.css(".location span[itemprop='location'] strong > text()").text
+    location = result.css(".location span[itemprop='location'] > text()").text
+    time = result.css(".location div > text()").text
+
+    artist = result.css("[itemprop='name performers']").first.text
+    
+    url = result.css("a.event").first["href"]
+
+    event = { 
+      artist: artist,
+      venue: venue,
+      location: location,
+      time: time,
+      date: { 
+        month_abbr: month_abbr,
+        month: month,
+        date: date,
+        day: day } ,
+      url: url
+    }
+
+    hash = "#{date}#{month}#{artist}"
+    new_events << hash
+
+    if !@redis.hexists(EVENTS_HASH_NAME, hash)
+      puts "Adding: #{hash}"
+      @redis.hset(EVENTS_HASH_NAME, hash, JSON.generate(event))
+    end
+  end
+
+  return new_events
+end
+
+def cleanup(new_events)
+  # Remove events that no longer exist
+  existing = @redis.hkeys(EVENTS_HASH_NAME)
+  diff = existing - new_events
+  
+  unless diff.empty?
+    puts "Removing: '#{diff.join("' '")}'}"
+    @redis.hdel(EVENTS_HASH_NAME, "'#{diff.join("' '")}'")
+  end
 end
 
 # Go to settlement page
@@ -39,34 +91,6 @@ seattle = Nokogiri::HTML.parse browser.html
 # Grab results
 results = seattle.css("#result_local #tbl_local tr")
 
-results.each do |result|
-  month = result.css(".month abbr").first["title"]
-  date = result.css("td.date div.date").first.text
-  day = result.css(".day abbr").first["title"]
+new_events = process_results(results)
 
-  venue = result.css(".location span[itemprop='location'] strong > text()").text
-  location = result.css(".location span[itemprop='location'] > text()").text
-  time = result.css(".location div > text()").text
-
-  artist = result.css("[itemprop='name performers']").first.text
-  
-  url = result.css("a.event").first["href"]
-
-  event = { 
-    artist: artist,
-    venue: venue,
-    location: location,
-    time: time,
-    date: { 
-      month: month,
-      date: date,
-      day: day } ,
-    url: url
-  }
-
-  hash = "#{date}#{month}#{artist}"
-
-  if !redis.hexists(EVENTS_HASH_NAME, hash)
-    redis.hset(EVENTS_HASH_NAME, hash, JSON.generate(event))
-  end
-end
+cleanup(new_events)
